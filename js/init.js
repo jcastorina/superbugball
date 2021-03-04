30c28210fdf1b33268bc825bcd6906c0eaabd127
Joseph Castorina <jcastorina@gmail.com>
2021-03-03 23:20:41 -0700

    second commit :)

--- js/init.js
+++ js/init.js
@@ -5,5 +5,5 @@ const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 
 //renderer.setSize(400 * (16 / 9), 400);
-renderer.setSize(h, w);
+renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
